7842259d86bae7c72c83eeaae9fada813fadf707
Ken Durkin <kdurkin6@fordham.edu>
2015-04-12 04:32:31 -0400

    Made OAuth my bitch

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var app = express();
 
+var twitterAPI = require('node-twitter-api');
+var twitter = new twitterAPI({
+    consumerKey: 'CFRzw4QBDkxDua8SFmbKuKqlh',
+    consumerSecret: '2b5ZBt5tRRysTPQpLxG78u6wTjFRog1H68AyZNFyaiyMj5TsHn',
+    callback: 'http://127.0.0.1:3000/login'
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -14,10 +21,69 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+    secret: 'dev',
+    resave: true,
+    saveUninitialized: true
+}));
+
+var rToken = '';
+var rTokenSecret = '';
+
+var requestToken = function(req, res, next) {
+    twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+        if (error) {
+            error = JSON.stringify(error);
+            console.log("Error getting OAuth request token : " + error);
+        } else {
+            //store token and tokenSecret somewhere, you'll need them later; redirect user 
+            rToken = requestToken;
+            rTokenSecret = requestTokenSecret;
+            console.log("Access Token & Secret:\n"+rToken+ '\n' + rTokenSecret);
+            res.redirect("https://twitter.com/oauth/authenticate?oauth_token=" + rToken);
+        }
+    });
+};
+
+var accessToken = function(req,res,next) {
+    if(!req.query.oauth_token || !req.query.oauth_verifier) {
+        res.sendFile(__dirname + '/views/error.html');
+    }
+
+    console.log("REQUEST TOKEN IS " + rToken);
+    var requestToken = rToken;
+    var requestTokenSecret = rTokenSecret;
+    var oauth_verifier = req.query.oauth_verifier;
+
+    twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+        if (error){
+            error = JSON.stringify(error);
+            console.log("Error getting Access token : " + error);
+        } else {
+            console.log("Authorized");
+            twitter.verifyCredentials(accessToken, accessTokenSecret, function(error, data, results) {
+                if(error) {
+                    error = JSON.stringify(error);
+                    console.log("Error getting User info : " + error);
+                } else {
+                    console.log("Logged in as " + data.screen_name)
+                }
+            });
+        }
+    });
+};
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-      res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res, next) {
+    requestToken(req,res, next);
+    //res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', function (req, res, next) {
+    accessToken(req,res, next);
+    res.sendFile(__dirname + '/views/login.html');
 });
 
 // catch 404 and forward to error handler
