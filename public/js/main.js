5cc9e704e7a038f5690874b18aabb1a67d754863
Henry Majoros <majoros.henry@gmail.com>
2015-04-12 08:05:28 -0400

    fun for batman, not for babies. also im done

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,63 @@
 (function() {
 
     $(document).ready(function() {
-        renderTopColors();
+        renderLoginScreen();
+
+        $(window).on('resize', renderResize);
+
+        $('.btn-login').on('click', renderMainScreen);
+        $('.nav-btn').on('click', openMenu);
+        $('.close-btn').on('click', closeMenu);
     });
 
-    function renderTopColors() {
-        var sections = $('.border-section'),
-            colors = ['#B2DFDB', '#80CBC4', '#4DB6AC', '#26A69A', '#009688', '#00897B', '#00796B', '#00695C', '#004D40'];
+    function renderResize() {
+        var h = $(window).height(),
+            w = $(window).width();
 
-        for (var i = 0; i < sections.length / 2; i++) {
-            
-        }
+        $('.sidebar').css({ height: h });
+    }
+
+    function renderLoginScreen() {
+        var h = $(window).height(),
+            w = $(window).width(),
+            cnt = $('.login > .row > div'),
+            cntHeight = cnt.height();
+
+        $('.splash').css({ height: h, width: w });
+        cnt.css({ marginTop: (h - cntHeight) / 2 });
+        
+        renderResize();
+    }
+
+    function renderMainScreen() {
+        $('.content').css({ opacity: 0 });
+
+        $('.logo').animate({
+            height: 96,
+            width: 96
+        }, 250, function() {
+            $('.content').removeClass('hide').delay(200).animate({ opacity : 1 }, 500);
+        });
+
+        $('.login').addClass('hide');
+        // $('.content').removeClass('hide');
+        $('.logo').removeClass('splash');
+    }
+
+    function openMenu() {
+        var w = $(window).width();
+
+        $('body').css({ overflow: 'hidden' });
+        $('body').animate({ marginLeft: 200 }, 250);
+        $('.logo').animate({ left: (w / 2) + 150 });
+    }
+
+    function closeMenu() {
+        var w = $(window).width();
+
+        $('body').css({ overflow: 'auto' });
+        $('body').animate({ marginLeft: 0 }, 250);
+        $('.logo').animate({ left: '50%' });
     }
 
 })();
